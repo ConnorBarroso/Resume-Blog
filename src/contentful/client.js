ad8056a124d52e5a18bc3c161c08cbf9c5d9213d
ConnorBarroso <connor.barroso@gmail.com>
2021-08-28 18:35:12 -0400

    final changes

--- src/contentful/client.js
+++ src/contentful/client.js
@@ -3,4 +3,5 @@ import * as contentful from 'contentful'
 export const client = contentful.createClient({
     space: 'mh11o5zx6qwn',
     accessToken: '1Hs8o5O9NcP9ABGoCz1qEK_zwpstVF5Mk4Fl5G2FIec' 
+    
 })
