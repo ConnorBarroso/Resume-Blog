d58440f708ed976f86c6b4e92434ebfd4ab27c06
ConnorBarroso <connor.barroso@gmail.com>
2021-10-22 13:38:29 -0400

    refreshing links

--- src/components/Home-page/home-page.component.jsx
+++ src/components/Home-page/home-page.component.jsx
@@ -59,7 +59,7 @@ const Homepage = ()=>{
                 <p className='intro-des'>I am a React.js front end developer. I am building this blog to demonstrate my portfolio and to show how my skills in development change and mature over my career.</p>
                 <a className='resume-button'href='https://docs.google.com/document/d/1J1gie1sOwt2gH3p6SCPYE78fcxxri6Hv4fDzG8xBzhU/edit?usp=sharing' target="_blank" rel="noreferrer">View my Resume</a>
             </div>
-            {  (<img className='portrait' alt='portrait' src='https://scontent.fymq2-1.fna.fbcdn.net/v/t1.6435-9/203434578_1881649052014463_5108897948529942915_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0QHqg9ePrJ4AX98e8Xv&_nc_ht=scontent.fymq2-1.fna&oh=eff954bda5966441c9f770b45b6a5bf6&oe=61694E1D'/>)}
+            {  (<img className='portrait' alt='portrait' src='https://scontent.fymq2-1.fna.fbcdn.net/v/t1.6435-9/203434578_1881649052014463_5108897948529942915_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=4i0ORZUiS2EAX9XwvkU&_nc_ht=scontent.fymq2-1.fna&oh=df65860c6f4c47d054a9915c5552b1ea&oe=6198C41D'/>)}
             </div>
             <div className='div-2'>
                 <div className='recent-con'>
--- src/components/footer-component/footer.component.jsx
+++ src/components/footer-component/footer.component.jsx
@@ -9,7 +9,7 @@ const Footer = ()=>{
         <div className='footer-con'>
             <div className='link-con'>
                 <a href='https://ca.linkedin.com/in/connor-barroso-926513217?' target="_blank" rel="noreferrer"><img className='link-img' src={LinkedinLogo} alt='linkedin logo'/></a>
-                <a href='mailto:connor.barroso@gmail.com' id='margin-left' target="_blank" rel="noreferrer"><img className='link-img' src='https://scontent.fymq2-1.fna.fbcdn.net/v/t1.15752-9/241903693_257948099520310_8324435721388279776_n.png?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=gSBlGuCeclUAX9VAPOr&tn=WP8ApEKM8L91Iq-F&_nc_ht=scontent.fymq2-1.fna&oh=4f51b4006549720057d0cc27a7c25a33&oe=61686633' alt='email logo'/></a>
+                <a href='mailto:connor.barroso@gmail.com' id='margin-left' target="_blank" rel="noreferrer"><img className='link-img' src='https://scontent.fymq2-1.fna.fbcdn.net/v/t1.15752-9/241903693_257948099520310_8324435721388279776_n.png?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_ohc=4QfoBU-0oX4AX_221-G&_nc_ht=scontent.fymq2-1.fna&oh=b8143c72a4bcf2380b7c4b1f13abd543&oe=6197DC33' alt='email logo'/></a>
                 <a href='https://github.com/ConnorBarroso' id='margin-left' target="_blank" rel="noreferrer"><img className='link-img' src='https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-github-3.png&r=33&g=36&b=61' alt='github logo'/></a>
             </div>
             <div className='copy-con'>Design by Tinjo Thomas</div>
